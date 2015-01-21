d9b359b9f0a36d66da3ac3b834c0dc18c55edd88
alekzer <alekzer@gmail.com>
2015-01-21 13:09:54 +0100

    ajout main

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fonction.h"
+
+
+
+int main()
+{
+    scanf ("%d",&nombre);
+    triple (&nombre);
+    printf("le nombre est %d\n", nombre);
+    return 0;
+}
